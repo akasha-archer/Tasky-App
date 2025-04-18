a8cd0a8aac7c33817925745c8c620b22ed61e8cb
Akasha Archere <akasha.archer@gmaile.com>
2025-04-17 21:50:46 -0400

    Add api functions

---
+++ app/src/main/java/com/example/taskyapplication/remote/TaskyApiService.kt
@@ -0,0 +1,43 @@
+package com.example.taskyapplication.remote
+
+import com.example.taskyapplication.auth.LoggedInUserResponse
+import com.example.taskyapplication.auth.UserAccessTokenResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface TaskyApiService {
+
+    @POST("/register")
+    suspend fun registerUser(
+        @Header("x-api-key") apiKey: String,
+        @Body fullName: String,
+        @Body email: String,
+        @Body password: String
+    )
+
+    @POST("/login")
+    suspend fun loginExistingUser(
+        @Header("x-api-key") apiKey: String,
+        @Body email: String,
+        @Body password: String
+    ): LoggedInUserResponse
+
+    @POST("/accessToken")
+    suspend fun getNewAccessToken(
+        @Header("x-api-key") apiKey: String,
+        @Body refreshToken: String,
+        @Body userId: String
+    ): UserAccessTokenResponse
+
+    @GET("/authenticate")
+    suspend fun authenticateUser(
+        @Header("x-api-key") apiKey: String
+    ): Result<String>
+
+    @GET("/logout")
+    suspend fun logoutUser(
+        @Header("x-api-key") apiKey: String
+    )
+}
