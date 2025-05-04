e6aea5b6c54f8cf3303249ead1e262a16b42fdc9
Akasha Archere <akasha.archer@gmaile.com>
2025-05-04 15:32:27 -0400

    delete unused imports

--- app/src/main/java/com/example/taskyapplication/auth/presentation/utils/AuthHelperFunctions.kt
+++ app/src/main/java/com/example/taskyapplication/auth/presentation/utils/AuthHelperFunctions.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.taskyapplication.R
 import com.example.taskyapplication.ui.theme.TaskyDesignSystem.Companion.taskyColors
