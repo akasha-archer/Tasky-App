da0eea3853c6eeae71346b8d4deb8b6c901fc66c
Akasha Archere <akasha.archer@gmaile.com>
2025-05-04 15:32:27 -0400

    delete unused imports

--- app/src/main/java/com/example/taskyapplication/auth/presentation/components/AccountCreationContent.kt
+++ app/src/main/java/com/example/taskyapplication/auth/presentation/components/AccountCreationContent.kt
@@ -1,19 +1,8 @@
 package com.example.taskyapplication.auth.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.taskyapplication.auth.domain.RegisterUserState
 import com.example.taskyapplication.auth.domain.UserLoginData
 
