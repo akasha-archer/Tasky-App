511950094522f0582fb5de4814028de80c152255
akasha-archer <akasha.archer@nytimes.com>
2025-05-20 16:36:39 -0400

    delete unused imports

--- app/src/main/java/com/example/taskyapplication/agenda/task/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/example/taskyapplication/agenda/task/data/local/dao/TaskDao.kt
@@ -1,11 +1,9 @@
 package com.example.taskyapplication.agenda.task.data.local.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.taskyapplication.agenda.task.data.local.entity.TaskEntity
-import com.example.taskyapplication.agenda.task.presentation.TaskUiState
 
 @Dao
 interface TaskDao {
