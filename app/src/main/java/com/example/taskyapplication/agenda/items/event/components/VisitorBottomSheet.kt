db06846e48cc441a4a2b5b17fb5e5b5154d872fe
Akasha Archere <akasha.archer@gmaile.com>
2025-06-09 16:33:30 -0400

    create attendee bottom sheet

---
+++ app/src/main/java/com/example/taskyapplication/agenda/items/event/components/VisitorBottomSheet.kt
@@ -0,0 +1,190 @@
+package com.example.taskyapplication.agenda.items.event.components
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberStandardBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalWindowInfo
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskyapplication.R
+import com.example.taskyapplication.agenda.presentation.components.AgendaScreenDivider
+import com.example.taskyapplication.ui.theme.TaskyDesignSystem.Companion.taskyColors
+import com.example.taskyapplication.ui.theme.TaskyTypography
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddAttendeeBottomSheet(
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    isButtonEnabled: Boolean = false,
+    isInvalidEmail: Boolean = false,
+    onAddAttendee: () -> Unit = {},
+    onCancelAddAttendee: () -> Unit = {},
+) {
+    val screenWidth = LocalWindowInfo.current.containerSize.width.dp
+
+    ModalBottomSheet(
+        modifier = modifier,
+        onDismissRequest = { onCancelAddAttendee() },
+        sheetState = rememberStandardBottomSheetState(
+            initialValue = androidx.compose.material3.SheetValue.Expanded,
+        ),
+        containerColor = taskyColors.surface,
+        content = {
+            Column(
+                modifier = Modifier
+                    .background(color = taskyColors.surface)
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(vertical = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        modifier = Modifier.weight(2f),
+                        text = stringResource(R.string.add_visitor).uppercase(),
+                        style = TaskyTypography.labelMedium.copy(
+                            color = taskyColors.primary,
+                            fontSize = 18.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "close bottom sheet",
+                        tint = taskyColors.primary,
+                        modifier = Modifier
+                    )
+                } // end of row
+                AgendaScreenDivider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .requiredWidth(screenWidth + 32.dp)
+                        .padding(bottom = 24.dp)
+                )
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    value = "",
+                    onValueChange = {},
+                    shape = RoundedCornerShape(12.dp),
+                    supportingText = {
+                        if (isInvalidEmail) {
+                            Text(
+                                modifier = Modifier.padding(bottom = 8.dp),
+                                text = stringResource(R.string.add_attendee_error_message),
+                                style = TaskyTypography.bodySmall.copy(
+                                    color = taskyColors.error,
+                                )
+                            )
+                        }
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = taskyColors.surfaceHigh,
+                        unfocusedContainerColor = taskyColors.surfaceHigh,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedLabelColor = Color.Transparent,
+                        unfocusedLabelColor = Color.Transparent,
+                        errorIndicatorColor = taskyColors.error
+                    ),
+                    placeholder = {
+                        Text(
+                            modifier = Modifier.padding(vertical = 16.dp),
+                            text = stringResource(R.string.event_email_hint),
+                            style = TaskyTypography.bodyMedium.copy(
+                                color = taskyColors.onSurfaceVariant
+                            )
+                        )
+                    }
+                )
+                Button(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(96.dp)
+                        .padding(start = 20.dp, end = 20.dp, top = 16.dp, bottom = 16.dp)
+                        .clip(RoundedCornerShape(25.dp)),
+                    onClick = onAddAttendee,
+                    enabled = isButtonEnabled,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = taskyColors.primary,
+                        contentColor = taskyColors.onPrimary,
+                        disabledContainerColor = taskyColors.onSurfaceVariant.copy(
+                            alpha = 0.7f,
+                        )
+                    )
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .size(36.dp)
+                                .alpha(if (isLoading) 1f else 0f),
+                            strokeWidth = 1.5.dp,
+                            color = taskyColors.onPrimary
+                        )
+                        Text(
+                            modifier = Modifier
+                                .alpha(if (isLoading) 0f else 1f),
+                            text = stringResource(R.string.add_visitor_button).uppercase(),
+                            color = taskyColors.onPrimary,
+                            style = TaskyTypography.labelMedium
+                        )
+                    }
+                }
+
+
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddAttendeeBottomSheetPreview() {
+    AddAttendeeBottomSheet()
+}
